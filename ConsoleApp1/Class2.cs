b3c7c42058f69fd54351154bd81656775188ebc9
P301 <P301>
2017-05-15 21:30:19 +0400

    first commit

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp1;
+
+namespace Rcard_ns
+{
+    class Rcard
+    {       
+            public int vcard_number;
+            public string userName="Jack";
+            private int password;
+            public int balance=500;
+           
+            public int createCardNumber1()
+            {
+                Random rnd1 = new Random();
+                int card_number1 = rnd1.Next(1000, 2500);                
+                return card_number1;
+            }
+        public int createCardNumber2()
+        {
+            Random rnd2 = new Random();
+            int card_number2 = rnd2.Next(2500, 5000);
+            return card_number2;
+        }
+        public int createCardNumber3()
+        {
+            Random rnd3 = new Random();
+            int card_number3 = rnd3.Next(5000, 7500);
+            return card_number3;
+        }
+
+        public int createCardNumber4()
+        {
+            Random rnd4 = new Random();
+            int card_number4 = rnd4.Next(7500, 9999);
+            return card_number4;
+        }
+
+
+        public int createPassword()
+            {           
+                Random rnd = new Random();
+                int pin = rnd.Next(100, 999);
+                password = pin;
+                return password;
+            }
+       public DateTime date()
+        {
+            DateTime value = new DateTime(2018, 1, 18);
+            return value;
+        }  
+
+    }
+    
+    }
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rcard_ns;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rcard mycard = new Rcard();
+            Console.WriteLine("Card number: "+mycard.createCardNumber1()+ "-"+ mycard.createCardNumber2() + "-"+ mycard.createCardNumber3() + "-"+ mycard.createCardNumber4());
+            Console.WriteLine("Card password: "+mycard.createPassword());
+            Console.WriteLine("User name: "+mycard.userName);
+            Console.WriteLine("Card balance: "+mycard.balance);
+            Console.WriteLine("Card Date: " + mycard.date());
+            Console.ReadLine();
+        }
+    }
+}
